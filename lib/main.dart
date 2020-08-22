1151627a329324d25ff61cada9a985de60b7c30a
zeeshan_progs <md.zeeshaniqbal7277@gmail.com>
2020-08-22 22:35:23 +0530

    100 Days Of Flutter update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practiceproject/Day5/NewDrawer.dart';
+import 'package:practiceproject/Day6/NewCurved.dart';
 
 void main() {
   runApp(
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: NewDrawer(),
+      home: CurvedNavigation(),
     );
   }
 }
