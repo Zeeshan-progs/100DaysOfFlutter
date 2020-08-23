89b057cec569c7fffd84c15beb4cd87ea5f09d8e
zeeshan_progs <md.zeeshaniqbal7277@gmail.com>
2020-08-23 23:57:11 +0530

    100 Days Of Flutter update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practiceproject/Day6/NewCurved.dart';
+import 'package:practiceproject/Day7/Alert_Box.dart';
 
 void main() {
   runApp(
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CurvedNavigation(),
+      home: NewAlert(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
