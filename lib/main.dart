bf1b46dc70fddc0f5276341ae8f7f0cc43ecfc5c
zeeshan_progs <md.zeeshaniqbal7277@gmail.com>
2020-08-25 23:28:59 +0530

    100 Days Of Flutter update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practiceproject/Day7/Alert_Box.dart';
+import 'package:practiceproject/Day8/ImageSlider.dart';
 
 void main() {
   runApp(
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: NewAlert(),
+
+      home: ImageSlider(),
       debugShowCheckedModeBanner: false,
     );
   }
